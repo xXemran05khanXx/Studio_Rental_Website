82f113e27f1d08a571414893e05846bee3b61642
Imran Ahmed Khan <144812599+xXemran05khanXx@users.noreply.github.com>
2024-09-22 13:19:07 +0000

    Register functionality, MongoDB, Prisma setup with 2 Unresolved Error "[...nextauth].js"

---
+++ studio_rental_web/app/api/register/route.ts
@@ -0,0 +1,25 @@
+import prisma from "@/app/libs/prismadb"; 
+import { NextResponse } from "next/server"; 
+import bcrypt from "bcrypt"; 
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json(); 
+
+    const { email, name, password } = body; 
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to create user" }, { status: 500 });
+  }
+}
