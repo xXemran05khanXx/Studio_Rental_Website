78ca3a3df8122849e2ea5f82e1b4ce690f23c67e
Imran Ahmed Khan <144812599+xXemran05khanXx@users.noreply.github.com>
2024-09-26 12:29:17 +0000

    feat: Complete listing creation and fetching features
    - Implemented step 5: Added description and price fields, and established POST route for listing creation.
    - Added functionality to fetch listings using server components, including Listing Card component and direct server actions.

---
+++ studio_rental_web/app/actions/getListing.ts
@@ -0,0 +1,15 @@
+
+export default async function getListing () {
+    try {
+
+        const listings = await prisma?.listing.findMany({
+
+          orderBy: {
+            createdAt: 'desc'
+          } 
+        });
+     return listings || [];
+    } catch (error: any) {
+        throw new Error(error);
+    }
+}
--- studio_rental_web/app/page.tsx
+++ studio_rental_web/app/page.tsx
@@ -1,7 +1,47 @@
-import Image from "next/image";
+import getCurrentUser from "./actions/getCurrentUser";
+import getListing from "./actions/getListing";
+import  Container  from "./components/Container";
+import ListingCard from "./components/listings/ListingCard";
+import EmptyState from "./components/navbar/EmptyState";
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListing();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <EmptyState showReset />
+    )
+  }
   return (
-    <div className="text-rose-500 text-2xl">Studio Rental Website</div>
+    <Container>
+      <div
+        className="
+         pt-24
+         grid
+         grid-cols-1
+         sm:grid-cols-2
+         md:grid-cols-3
+         lg:grid-cols-4
+         xl:grid-cols-5
+         2xl:grid-cols-6
+         gap-8
+        "
+      >
+       {listings.map((listing:any)=>{
+        return (
+           <ListingCard 
+           
+           currentUser = {currentUser}
+
+           key= { listing.id } 
+
+           data= { listing }
+            
+           />
+        )
+       })}
+     </div>
+    </Container>
   );
 }
