7061ae682c0f4cda2907f66136e5ffd169ad7e33
imran khan <xemran05khax@gmail.com>
2024-10-08 21:57:43 +0530

    feat: fixed import statements, reformatted code, and added user greeting

--- studio_rental_web/app/components/navbar/Navbar.tsx
+++ studio_rental_web/app/components/navbar/Navbar.tsx
@@ -1,8 +1,8 @@
-'use client'
+'use client';
 
-import  Container  from "../Container";
+import Container from "../Container";
 import Categories from "./Categories";
-import Logo from "./logo"
+import Logo from "./logo"; // Ensure the file name matches the case
 import { Search } from "./Search";
 import UserMenu from "./UserMenu";
 import React from 'react';
@@ -13,30 +13,25 @@ interface NavbarProps {
     currentUser?: SafeUser | null;
 }
 
-const Navbar: React. FC<NavbarProps> = ({currentUser}) => {
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
     return (
         <div className="fixed w-full bg-white z-10 shadow-sm">
             <div className="py-4 border-b-[1px]">
                 <Container>
-                    <div
-                     className="
-                     flex
-                     flex-row
-                     items-center
-                     justify-between
-                     gap-3
-                     md:gap-0">
-                     
-                     <Logo/>  
-                     <Search/>
-                     <UserMenu currentUser={currentUser}/>
-                    </div>  
+                    <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+                        <Logo />
+                        <Search />
+                        <div className="flex items-center px-7">
+                            <span className="font-bold  ">Welcome </span>
+                            <span className="ml-1 text-gray-500">{currentUser?.name || 'Guest'}</span>
+                        </div>
+                        <UserMenu currentUser={currentUser} />
+                    </div>
                 </Container>
-
             </div>
             <Categories />
         </div>
     );
 }
 
-export default Navbar
+export default Navbar;
